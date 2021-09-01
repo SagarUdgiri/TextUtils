401b49869bcbbee512a2040eb24bafb9231d43eb
Sagar Udgiri <udgirisagar36@gmail.com>
2021-09-01 15:42:26 +0530

    Enabling Dark/Light Mode

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import './App.css';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, { useState } from 'react'
 
 function App() {
   // const titles=["Home","About","Contact"];
   function searched() {
     alert("fdj")
   }
+  function toggleMode() {
+    if (Mode === 'light')
+    {
+      setMode('dark')
+      document.body.style.backgroundColor='grey'
+    }
+    else
+    {
+      setMode('light')    
+      document.body.style.backgroundColor='white'
+    }
+  }
+  const [Mode, setMode] = useState('light');
   return (
     <div >
-      <Navbar title="TextUtils" search={searched}></Navbar>
+      <Navbar title="TextUtils" search={searched} mode={Mode} toggleMode={toggleMode}></Navbar>
       <div className="container my-3">  
-      <TextForm heading="Enter Text to Analyz"/>
+      <TextForm heading="Enter Text to Analyz" mode={Mode} toggleMode={toggleMode}/>
       {/* <About/> */}
       </div>
     </div>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -36,23 +36,27 @@ const TextForm = (props) => {
          settext(event.target.value)
      }
     return (
-        <div>
+        <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} id="myText" rows="5" onChange={handleonChange}></textarea>
+            <textarea className="form-control" value={text} id="myText" rows="5" onChange={handleonChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',
+            color:props.mode==='dark'?'white':'black'}}></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={ConverttoUpper}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={ConverttoLower}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-            <input type="text" className="mx-1" placeholder="Search for a Word" onChange={(event)=> setstr(event.target.value)}/><button className="btn btn-primary mx-1" onClick={searchString}>Search</button>
+            <input type="text" className="mx-1" placeholder="Search for a Word" onChange={(event)=> setstr(event.target.value)}
+            style={{backgroundColor:props.mode==='dark'?'grey':'white',
+            color:props.mode==='dark'?'white':'black'}}/>
+            <button className="btn btn-primary mx-1" onClick={searchString}>Search</button>
             <button className="btn btn-primary mx-1" onClick={()=> settext("")}>Clear Text</button>
             <div className="container my-3">
                 <h2>Your Text Summary</h2>
                 <p> {text.split(' ').length} Words and {text.length} Characters</p>
                 <p> {0.008 * text.split(' ').length} Minutes to read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Enter Some Text in Textarea'}</p>
             </div>
         </div>
     );
